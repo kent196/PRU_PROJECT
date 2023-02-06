19235cc56b08708a4eeacd1c41c1a1224f925759
Kent <kentdev196@gmail.com>
2023-02-06 14:36:29 +0700

    add collision

--- Assets/Sprites/Cainos/Pixel Art Top Down - Basic/Texture/TX Plant.tsx
+++ Assets/Sprites/Cainos/Pixel Art Top Down - Basic/Texture/TX Plant.tsx
@@ -1,4 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="TX Plant" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="TX Plant.png" width="512" height="512"/>
+ <tile id="192" probability="0.001"/>
+ <tile id="193" probability="0.001"/>
+ <tile id="194" probability="0.001"/>
+ <tile id="195" probability="0.001"/>
+ <tile id="208" probability="0.001"/>
+ <tile id="209" probability="0.001"/>
+ <tile id="210" probability="0.001"/>
+ <tile id="211" probability="0.001"/>
+ <tile id="224" probability="0.001"/>
+ <tile id="225" probability="0.001"/>
+ <tile id="226" probability="0.001"/>
+ <tile id="227" probability="0.001"/>
+ <tile id="240" probability="0.001"/>
+ <tile id="241" probability="0.001"/>
+ <tile id="242" probability="0.001"/>
+ <tile id="243" probability="0.001"/>
 </tileset>
