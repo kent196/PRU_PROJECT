e1710d3b8c04327d8457e4a5b3752736f413e8bd
cris nguyen <kingericvt96@gmail.com>
2023-02-09 16:16:41 +0700

    commit to pull

--- Assets/Scripts/Utils/CameraFollowPlayer.cs
+++ Assets/Scripts/Utils/CameraFollowPlayer.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class CameraFollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private Vector3 minValue, maxValue;
 
-    private float lerpSpeed = 2f;
-    private Vector3 offset;
-    private Vector3 playerPos;
+    private Vector3 playerPosition;
+
+    Vector3 offset;
+    // Start is called before the first frame update
     void Start()
     {
         offset = new Vector3(0, 0, -10);
+
     }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        playerPos = player.position + offset;
-        //transform.position = Vector3.Lerp(transform.position, playerPos, lerpSpeed * Time.deltaTime);
-        transform.position = playerPos;
+        playerPosition = player.position + offset;
+
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(playerPosition.x, minValue.x, maxValue.x),
+            Mathf.Clamp(playerPosition.y, minValue.y, maxValue.y),
+            Mathf.Clamp(playerPosition.z, minValue.z, maxValue.z));
+
+        transform.position = Vector3.Lerp(transform.position, boundPosition, 3 * Time.deltaTime);
     }
 }
