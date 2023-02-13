f1e2432ba136ce3ed3dfd8f6fe2685cf9c0d0865
cris nguyen <kingericvt96@gmail.com>
2023-02-13 11:07:16 +0700

    commit

---
+++ Assets/Sprites/Cainos/Pixel Art Top Down - Basic/Texture/collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="collision" tilewidth="32" tileheight="32" tilecount="375" columns="25">
+ <image source="../../../Tiled/collision.png" width="820" height="509"/>
+</tileset>
