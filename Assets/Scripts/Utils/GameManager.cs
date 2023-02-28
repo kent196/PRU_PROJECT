e93df05728745b3b1577e1870d0406020b9ab536
Kent <kentdev196@gmail.com>
2023-02-28 14:44:47 +0700

    power up UI

--- Assets/Scripts/Utils/GameManager.cs
+++ Assets/Scripts/Utils/GameManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private Animator animator;
+    public static GameManager Instance { get; private set; }
+    public GameObject powerUpSelect;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        powerUpSelect = GameObject.FindWithTag("PowerUpSelect");
+        powerUpSelect.SetActive(false);
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void PowerUpSelect()
+    {
+        Time.timeScale = 0f;
+        powerUpSelect.SetActive(true);
+    }
+
+    public void PowerUpSelected()
+    {
+        Time.timeScale = 1f;
+        powerUpSelect.SetActive(false);
     }
 }
